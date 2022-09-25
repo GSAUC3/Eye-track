82b1f794a94489797f466e1f8c1b8328f7d7c42c
Rajarshi Banerjee <rajarshiban13@gmail.com>
2022-09-25 17:13:04 +0530

    head movement reduced

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ cx = px = cy = py = 0
 
 faceMesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 
-FACTOR = 4
+FACTOR = 3
 ORIGIN = (220, 150)
 SMOOTHIE = 0.2
 
 centerx=centery=0
-
+firstRun = True
 while 1:
     _, img = cap.read()
 
@@ -29,44 +29,26 @@ while 1:
     height,width,_ = img.shape
     # print(type(landmarks[0].landmark[474:478]))
     # ids = [145,159,475,477]
-    cv2.rectangle(img, ORIGIN, (ORIGIN[0]+16*FACTOR, ORIGIN[1]+9*FACTOR), (0, 255, 255), 4)
     lefteye = [33,133]#,263,362
     if landmarks:
-        # for i in lefteye:
-        #     l0=landmarks[0].landmark[i]
-        #     x0,y0 = int(l0.x*width),int(l0.y*height)
-        #     cv2.circle(img,(x0,y0),3,(0,255,0))
-
-    
         l=landmarks[0].landmark[145]
         l1=landmarks[0].landmark[159]
-        # l2=landmarks[0].landmark[475]
-        # l3=landmarks[0].landmark[477]
-        
-    
         x,y = (l.x*width),(l.y*height)
         x1,y1 = (l1.x*width),(l1.y*height)
-        # x2,y2 = int(l2.x*width),int(l2.y*height)
-        # x3,y3 = int(l3.x*width),int(l3.y*height)
-
         centerx,centery=(x+x1)//2,(y+y1)//2
-        # center_x,center_y=(x2+x3)//2,(y2+y3)//2
-        cv2.circle(img,(int(centerx),int(centery)),3,(255,0,0))
-        # cv2.circle(img,(center_x,center_y),3,(0,0,255))
-        # print(centerx,centery)
-        # print((x0-centerx)**2+(y0-centery)*2)
-
+        cv2.circle(img,(int(centerx),int(centery)),1,(255,0,255))
+        if firstRun:
+            ORIGIN = (int(centerx-32),int(centery-18))
+            firstRun=False
+        cv2.rectangle(img, ORIGIN, (ORIGIN[0]+16*FACTOR, ORIGIN[1]+9*FACTOR), (0, 255, 255), 1)
         centerx = interp(centerx, (ORIGIN[0], ORIGIN[0]+16*FACTOR), (0, 1920))
-
         centery = interp(centery, (ORIGIN[1], ORIGIN[1]+9*FACTOR), (0, 1080))
-
         cx = px + (centerx-px)*SMOOTHIE
         cy = py + (centery-py)*SMOOTHIE
         # current location = (new location -past location)/factor
         if x < width and y < height:
             pg.moveTo(cx, cy)
         px,py = cx,cy
-
 
     cv2.imshow('Output',img)
     if cv2.waitKey(1)==27:
